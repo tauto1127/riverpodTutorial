0bae90d8d16e5070faf59fc70452c401dcafe94e
Takuto <tiharu717@gmail.com>
2023-10-25 15:02:36 +0900

    firstCommit

---
+++ lib/consumer.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'model/activity.dart';
+import 'provider.dart';
+
+/// The homepage of our application
+class Home extends ConsumerWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+      // Read the activityProvider. This will start the network request
+      // if it wasn't already started.
+        // By using ref.watch, this widget will rebuild whenever the
+        // the activityProvider updates. This can happen when:
+        // - The response goes from "loading" to "data/error"
+        // - The request was refreshed
+        // - The result was modified locally (such as when performing side-effects)
+        // ...
+    final AsyncValue<Activity> activity = ref.watch(activityProvider);
+
+    return Center(
+        /// Since network-requests are asynchronous and can fail, we need to
+        /// handle both error and loading states. We can use pattern matching for this.
+        /// We could alternatively use `if (activity.isLoading) { ... } else if (...)`
+      child: switch (activity) {
+        AsyncData(:final value) => Text('Activity: ${value.activity}'),
+        AsyncError() => const Text('Oops, something unexpected happened'),
+        _ => const CircularProgressIndicator(),
+      },
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'consumer.dart';
+
+void main() {
+  runApp(
+    const ProviderScope(
+      child: MainApp()
+    )
+  );
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Home(),
+        ),
+      ),
+    );
+  }
+}
